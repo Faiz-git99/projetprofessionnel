cef0eb6cb9bbaef5bb63c115dea88b3b7b9f9e7a
Nicia07 <niciahamidou74@gmail.com>
2025-03-26 15:58:57 +0300

    creation de barre de navigation

--- fruits-legumes/src/components/navbar.js
+++ fruits-legumes/src/components/navbar.js
@@ -5,11 +5,25 @@ function Navbar (){
     let titre = "Fruits et Legumes " 
 
     return(
-        <div className="fel-navbar">
+        // <div className="fel-navbar">
+        //   <img src={logo} alt="logo fruits et legumes" className='fel-logo'/>
+        //   {titre}
+        //   <a href="#"><i class="fa fa-fw fa-search"></i> A propos</a>
+        //   <a href="#"><i class="fa fa-fw fa-envelope"></i> Menu</a>
+        //   <a href="#"><i class="fa fa-fw fa-user"></i> Inscription</a>
+        // </div>
+        
+      
+        <div class="navbar">
           <img src={logo} alt="logo fruits et legumes" className='fel-logo'/>
-          {titre}
-          
+          <a href="#"><i class="fa fa-search"></i></a>
+          <i class="fa fa-fw fa-search"></i>
+          <input type="text" placeholder="Recherche un article, fruits ou legumes.."></input>
+          <a href="#"> Se connecter</a>
+          <a href="#"> Menu</a>
         </div>
+
+
     )
 }
 
